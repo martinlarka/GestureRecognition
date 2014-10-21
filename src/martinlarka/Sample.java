23233eaa6a1606a77af370effd74464bc50a026f
Martin Lärka <martin.larka@gmail.com>
2014-10-21 15:11:26 +0200

    Adding gestures with DenseInstance

--- src/martinlarka/Sample.java
+++ src/martinlarka/Sample.java
@@ -1,16 +1,18 @@
 package martinlarka;
 
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.Scanner;
+
+import net.sf.javaml.core.Dataset;
+import net.sf.javaml.core.DefaultDataset;
+import net.sf.javaml.core.DenseInstance;
 
 import com.leapmotion.leap.*;
 
-import net.sf.javaml.core.DenseInstance;
-import net.sf.javaml.core.Instance;
-
 class SampleListener extends Listener {
 	
-	Hand tempHand;
+	Dataset gestures = new DefaultDataset();
+	String value = null;
 	
     public void onInit(Controller controller) {
         System.out.println("Initialized");
@@ -36,54 +38,53 @@ class SampleListener extends Listener {
         
         //Get hands
         for(Hand hand : frame.hands()) {
-        	tempHand = hand;
             String handType = hand.isLeft() ? "Left hand" : "Right hand";
-            System.out.println("  " + handType + ", id: " + hand.id()
-                             + ", palm position: " + hand.palmPosition());
 
             // Get the hand's normal vector and direction
             Vector normal = hand.palmNormal();
             Vector direction = hand.direction();
 
-            // Calculate the hand's pitch, roll, and yaw angles
-            System.out.println("  pitch: " + Math.toDegrees(direction.pitch()) + " degrees, "
-                             + "roll: " + Math.toDegrees(normal.roll()) + " degrees, "
-                             + "yaw: " + Math.toDegrees(direction.yaw()) + " degrees");
-
             // Get arm bone
             Arm arm = hand.arm();
-            System.out.println("  Arm direction: " + arm.direction()
-                             + ", wrist position: " + toHandCoor(arm.wristPosition())
-                             + ", elbow position: " + toHandCoor(arm.elbowPosition()));
 
-            VectorInstance[] instances = new VectorInstance[24];
+            double[] instanceData = new double[20*3];
             int i = 0;
             
             // Get fingers
             for (Finger finger : hand.fingers()) {
-                System.out.println("    " + finger.type() + ", id: " + finger.id()
+                /*System.out.println("    " + finger.type() + ", id: " + finger.id()
                                  + ", length: " + finger.length()
-                                 + "mm, width: " + finger.width() + "mm");
+                                 + "mm, width: " + finger.width() + "mm"); */
 
                 //Get Bones
                 for(Bone.Type boneType : Bone.Type.values()) {
                     Bone bone = finger.bone(boneType);
-                    
-                    instances[i] = new VectorInstance(bone.direction());
-                    i++;
+                    //System.out.println(finger.type() + " "+ bone.type());
+                    Vector temp = bone.direction();
+                    instanceData[i] = (double)temp.getX();
+                    instanceData[i+1] = (double)temp.getY();
+                    instanceData[i+2] = (double)temp.getZ();
+                    i = i+3;
                 }
+            }
+            if (i == 60 && !value.equals("")) {
+            	System.out.println("Entering "+ (gestures.size() + 1)  +" gesture with value " + value);
+            	DenseInstance temp = new DenseInstance(instanceData, value);
+            	gestures.add(temp);
+            	value = "";
             }
 
         }
 
         if (!frame.hands().isEmpty()) {
-            System.out.println();
+            //System.out.println();
         }
     }
+
+	public void setValue(String val) {
+		this.value = val;
+	}
     
-    Vector toHandCoor(Vector vec) {
-    	return vec.minus(tempHand.palmPosition());
-    }
 }
 
 class Sample {
@@ -99,15 +100,19 @@ class Sample {
         
 
         // Keep this process running until Enter is pressed
-        System.out.println("Press Enter to quit...");
-        try {
-            System.in.read();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        System.out.println("Enter quit to quit...");
 
+        Scanner keyboard = new Scanner(System.in);
+		String value = "";
+		
+		while (!value.equals("quit")) {
+			System.out.println("enter a sign value");
+			value = keyboard.nextLine();
+			listener.setValue(value);
+		}
+		keyboard.close();
         // Remove the sample listener when done
-        controller.removeListener(listener);
+        //controller.removeListener(listener);
     }
 }
 
